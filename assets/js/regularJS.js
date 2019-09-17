93990f0dbf79229341f2fb7d21d04aa386460646
JC <trilamanila@MacBook-Pro-8.fios-router.home>
2019-09-17 02:14:07 -0500

    Added Pokemon DB

--- assets/js/regularJS.js
+++ assets/js/regularJS.js
@@ -1,3 +1,35 @@
+var pokemons = [
+    {
+      name: 'charmander',
+      type: 'fire',
+      hp: 39,
+      attack: 52,
+      defense: 39,
+      level: 1
+      img: 'http://www.smogon.com/dex/media/sprites/xy/charmander.gif'
+    },
+    {
+      name: 'bulbasaur',
+      type: 'grass',
+      hp: 45,
+      attack: 49,
+      defense: 49,
+      level: 1
+      img: 'http://www.smogon.com/dex/media/sprites/xy/bulbasaur.gif'
+    },
+    {
+        name: 'squirtle',
+        type: 'water',
+        hp: 44,
+        attack: 48,
+        defense: 65,
+        level: 1
+        img: 'http://www.smogon.com/dex/media/sprites/xy/squirtle.gif'
+      },
+    
+  ]
+
+
 var gameState = {
     userPokemon: '',
     rivalPokemon: ''
@@ -5,6 +37,7 @@ var gameState = {
 console.log(gameState)
 var pokemonsEl = document.querySelector('.select-screen').querySelectorAll('.character')
 console.log(pokemonsEl)
+var battleScreenEl = document.getElementById('battle-screen')
 var i = 0;
 
     while (i < pokemonsEl.length) {
@@ -63,29 +96,13 @@ var i = 0;
 
 // // pokemon
 // // create data for 3 different pokemons, with their names, type, weaknesses, health, and attack moves(name, attack stat, maximum)
-// var pokemons = [
-//     {
-//       name: 'charmander',
-//       type: 'fire',
-//       attack: 52,
-//       stamina: 39,
-//       level: 1
-//     },
-//     {
-//       name: 'charmander',
-//       type: 'fire',
-//       attack: 52,
-//       stamina: 39,
-//       level: 1
-//     },
-    
-//   ]
+
   
   
 //   var attack = 20;
 //   var level = 10;
 //   var stack = 1.3;
-//   var stamina = 39;
+//   var defense = 39;
   
 //   // create a formula for attacks
 //   console.log((attack * level ) * stack / 7)
@@ -94,7 +111,7 @@ var i = 0;
   
 //   // create a formula for health
 //   //HP = 0.20 x Sqrt(Pokemon_level) x (HP_base_stat)
-//   console.log(((0.20 * Math.sqrt(level)) * stamina) * 15)
+//   console.log(((0.20 * Math.sqrt(level)) * defense) * 15)
   
   
   
